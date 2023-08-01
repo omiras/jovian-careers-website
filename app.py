c86fbc5445cdc23fe1266bea90f9aca8748720db
omiras <oscar.miras@gmail.com>
2023-08-01 15:06:57 +0000

    addeda API JOBS

--- app.py
+++ app.py
@@ -1,10 +1,39 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [{
+  'id': 1,
+  'title': 'Data Analyst',
+  'location': "Benglaru, India",
+  'salary': 'Rs, 10,00,000'
+}, {
+  'id': 2,
+  'title': 'Data Analyst',
+  'location': "Delhi, India",
+  'salary': 'Rs, 15,00,000'
+}, {
+  'id': 3,
+  'title': 'Front End Engineer',
+  'location': "Remote",
+  'salary': 'Rs, 12,00,000'
+}, {
+  'id': 4,
+  'title': 'Back End Engineer',
+  'location': "San Francisco",
+  'salary': '$120,000'
+}]
+
+
 @app.route("/")
-def hello_world():
-  return "Hello, world!"
+def hello_jovian():
+  return render_template("home.html", jobs=JOBS)
+
+
+@app.route('/api/jobs')
+def get_jobs():
+  return jsonify(JOBS)
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
